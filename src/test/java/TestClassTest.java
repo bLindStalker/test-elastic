6f0cb19841018597ee308236c9db8eab012704f8
ayakiemnko <ayakimenko@playtika.com>
2018-05-06 16:37:30 +0300

    test elastic

---
+++ src/test/java/TestClassTest.java
@@ -0,0 +1,10 @@
+import org.elasticsearch.test.ESTestCase;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestClassTest extends ESTestCase {
+    @Test
+    public void test() {
+        System.out.println("test elastic");
+    }
+}
